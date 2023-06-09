3d559e79d7062d7837ad424a6259ad43fa280f38
Gelio <70667354+bigelio@users.noreply.github.com>
2023-06-09 07:55:51 +0200

    chore: debug

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,17 @@ zero_downtime_deploy() {
   old_container_id=$(docker ps -f name=$service_name -q | tail -n1)
 
   # bring a new container online, running new code  
-  # (nginx continues routing to the old container only)  
+  # (nginx continues routing to the old container only) 
+  echo "Started scaling the containers..." 
   docker-compose up -d --no-deps --scale $service_name=2 --no-recreate $service_name
 
   # wait for new container to be available  
+  echo "Checking for the id of new container..."
   new_container_id=$(docker ps -f name=$service_name -q | head -n1)
   new_container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $new_container_id)
-  curl --silent --include --retry-connrefused --retry 30 --retry-delay 1 --fail http://$new_container_ip:3000/ || exit 1
-
+  echo "Chicking if the app is reachable..."
+  curl --silent --include --retry-connrefused --retry 30 --retry-delay 1 --fail http://$new_container_ip:3001/ || exit 1
+  echo "Starting to remove the container..."
   # take the old container offline  
   docker stop $old_container_id
   docker rm $old_container_id
